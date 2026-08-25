94b0d6709ec47d9f6f0e4ecaf52d586fe7e14817
TheBunnyMan123 <bunny@tkbunny.net>
2026-08-24 21:30:26 -0500

    Initial Commit

---
+++ src/client/java/dev/beangal/assrpg/client/AssRPGClient.java
@@ -0,0 +1,10 @@
+package dev.beangal.assrpg.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AssRPGClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
