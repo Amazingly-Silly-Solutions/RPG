fd8b680e7dacdaddd7a2da393d0edc4ff4c219c5
TheBunnyMan123 <bunny@tkbunny.net>
2026-08-26 12:22:14 -0500

    fix: block liquid flow into protected chunks

--- src/main/java/dev/beangal/assrpg/AssRPGUtils.java
+++ src/main/java/dev/beangal/assrpg/AssRPGUtils.java
@@ -1,10 +1,47 @@
 package dev.beangal.assrpg;
 
 import dev.beangal.assrpg.registry.AssRPGCardinalComponents;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.ChunkAccess;
+
+import static dev.beangal.assrpg.AssRPG.LOGGER;
 
 public class AssRPGUtils {
     public static boolean isChunkProtected(ChunkAccess chunk) {
+        if (chunk == null) {
+            return false;
+        }
+
         return AssRPGCardinalComponents.PROTECTED.get(chunk).get();
+    }
+
+
+    public static boolean isChunkProtected(BlockPos pos, Level level) {
+        return isChunkProtected(level.getChunkSource().getChunk(SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()), false));
+    }
+
+    public static boolean nextToProtectedChunk(BlockPos pos, Level level) {
+        if (isChunkProtected(pos, level)) { LOGGER.info("prot"); return false; }
+
+        int[][] directions = {
+                {1, 0},  // East
+                {-1, 0}, // West
+                {0, 1},  // South
+                {0, -1}  // North
+        };
+
+        for (int[] direction : directions) {
+            LOGGER.info(pos.immutable().offset(direction[0], 0, direction[1]).toString());
+            if (isChunkProtected(pos.immutable().offset(direction[0], 0, direction[1]), level)) {
+                LOGGER.warn(pos.immutable().offset(direction[0], 0, direction[1]).toString());
+                return true;
+            }
+        }
+
+        LOGGER.warn("done");
+
+        return false;
     }
 }
---
+++ src/main/java/dev/beangal/assrpg/mixin/WaterFluidMixin.java
@@ -0,0 +1,30 @@
+package dev.beangal.assrpg.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static dev.beangal.assrpg.AssRPGUtils.isChunkProtected;
+import static dev.beangal.assrpg.AssRPGUtils.nextToProtectedChunk;
+
+@Mixin(FlowingFluid.class)
+public abstract class WaterFluidMixin {
+    @Inject(method = "spreadTo", at = @At("HEAD"), cancellable = true)
+    private void cancelLiquidSpread(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, Direction direction, FluidState fluidState, CallbackInfo ci) {
+        if (!(levelAccessor instanceof ServerLevel serverLevel)) { return; }
+
+        BlockPos prevPos = blockPos.relative(direction.getOpposite());
+
+        if (nextToProtectedChunk(prevPos, serverLevel) && isChunkProtected(blockPos, serverLevel)) {
+            ci.cancel();
+        }
+    }
+}
