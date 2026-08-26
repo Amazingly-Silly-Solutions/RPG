9ce1c8a4bafebedb68b6c7f4a485cf2aee1a9e12
TheBunnyMan123 <bunny@tkbunny.net>
2026-08-26 10:34:20 -0500

    fix: fire mixin didn't mix into fire

--- src/main/java/dev/beangal/assrpg/mixin/FireBlockMixin.java
+++ src/main/java/dev/beangal/assrpg/mixin/FireBlockMixin.java
@@ -4,14 +4,14 @@ import dev.beangal.assrpg.AssRPGUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.PointedDripstoneBlock;
+import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PointedDripstoneBlock.class)
+@Mixin(FireBlock.class)
 public class FireBlockMixin {
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     private void preventBreakInProtectedChunks(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource, CallbackInfo ci) {
